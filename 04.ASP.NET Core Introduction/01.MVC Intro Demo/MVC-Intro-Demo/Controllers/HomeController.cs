7db658c30a7976f7e8930b0eec32cbab58fed301
Rositsa Nenova <rositsa.a.nenova@gmail.com>
2023-04-23 20:22:24 +0300

    added 04 exercises

---
+++ 04.ASP.NET Core Introduction/01.MVC Intro Demo/MVC-Intro-Demo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Diagnostics;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            ViewBag.Message = "This is an ASP.NET Core MVC app.";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SoftUni-17-CSharp-ASP-NET-Fundamentals
+
+You can find solutions to all of the exercises from the C# ASP.NET Fundamentals course from SoftUni.
+
+Any feedback would be appreciated and feel free to borrow some code if you can't think of anything else. :)
+
+May the Force be with you!
+
+## 4 ASP.NET Core MVC Introduction
+4.01. MVC Intro Demo<br>
